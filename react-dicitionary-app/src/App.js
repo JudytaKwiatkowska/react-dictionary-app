8070173553a462e68ac4358f53207b4cf932f854
JudytaKwiatkowska <kwiatkowska.judyta@gmail.com>
2022-10-08 15:47:26 +0200

    Added dictionary api integration

--- react-dicitionary-app/src/App.js
+++ react-dicitionary-app/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">Dictionary Search App</header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">Coded by Judyta Kwiatkowska</footer>
     </div>
   );
 }
